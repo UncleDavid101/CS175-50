0756880ff2e06e7beae22fa56d307f75e66cafd8
UncleDavid101 <146228763+UncleDavid101@users.noreply.github.com>
2024-09-03 12:45:21 -0400

    Add files via upload

---
+++ myInfo.py
@@ -0,0 +1,10 @@
+# CS175-50
+
+#Your first name and Last name
+
+#myInfo
+
+print("David Thomas II")
+print("633 Martin Luther King jr blvd, Newark, New Jersey, 07102")
+print("7326931601")
+print("Software engineering")
