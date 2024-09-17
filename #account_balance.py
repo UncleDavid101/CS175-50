cbff92b6b8ac633619287e8ba8ab1dadbc55ac67
UncleDavid101 <146228763+UncleDavid101@users.noreply.github.com>
2024-09-17 12:08:24 -0400

    Add files via upload

---
+++ #account_balance.py
@@ -0,0 +1,11 @@
+#account_balance.py
+#David Thomas II
+
+account = 1000
+account = account + (account * 0.5)
+print(f'After one year of interest your total balance is: ${account}')
+account = account + (account * 0.5)
+print(f'After two years of interest your total balance is: ${account}')
+account = account + (account * 0.5)
+print(f'After three years of interest your total balance is: ${account}')
+
