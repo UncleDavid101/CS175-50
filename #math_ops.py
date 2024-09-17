9744dc46d4420946b5e120a0b151dd815d8d3226
UncleDavid101 <146228763+UncleDavid101@users.noreply.github.com>
2024-09-17 12:16:45 -0400

    Add files via upload

---
+++ #math_ops.py
@@ -0,0 +1,10 @@
+#math_ops.py
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(f'The sum of the two numbers is {num1 + num2}')
+print(f'The difference of the two numbers is {num1 - num2}')
+print(f'The product of the two numbers is {num1 * num2}')
+print(f'The average of the two numbers is {(num1 + num2) / 2 }')
+print(f'The distance of the two numbers is {abs(num1 - num2)}')
+print(f'The maximum of the two numbers is {max(num1, num2)}')
+print(f'The minimum of the two numbers is {min(num1, num2)}')
