e27f31e0316361f37710684b57af0a279a0b0f7b
UncleDavid101 <146228763+UncleDavid101@users.noreply.github.com>
2024-09-19 15:33:14 -0400

    Add files via upload

---
+++ #print_grid.py
@@ -0,0 +1,31 @@
+#print_grid.py
+#David Thomas
+def comb():
+    print('+', end='')
+    print('-'*2, end='')
+    print('+', end='')
+    print('-'*2, end='')
+    print('+', end='')
+    print('-'*2, end='')
+    print('+')
+    print('|', end='')
+    print(' '*2, end='')
+    print('|', end='')
+    print(' '*2, end='')
+    print('|', end='')
+    print(' '*2, end='')
+    print('|')
+   
+def bottom():
+    print('+', end='')
+    print('-'*2, end='')
+    print('+', end='')
+    print('-'*2, end='')
+    print('+', end='')
+    print('-'*2, end='')
+    print('+')
+
+comb()
+comb()
+comb()
+bottom()
